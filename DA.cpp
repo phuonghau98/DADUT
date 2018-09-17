2604e63fc2cfd76274d2f5d183de9d31cb472dfb
phuonghau98 <42571892+phuonghau98@users.noreply.github.com>
2018-09-17 10:02:32 -0300

    Update DA.cpp
    Phuong them ham stringl, centerstr, clrscr

--- DA.cpp
+++ DA.cpp
@@ -1,4 +1,11 @@
 #include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+
+void clrscr()
+{
+    system("@cls||clear");
+}
 // Su dung C
 // viet cac chuc nang duoi dang ham, su dung con tro de thay doi gia tri trong mang nhan vien
 
@@ -20,14 +27,33 @@ struct employee {
 // function : xoa cac nhan vien lon hon 50 tuoi
 // function : tinh luon, phu cap, thuc linh theo quy uoc trong file .doc
 // function : in ra bang thong ke theo don vi gom cac cot: don vi, so nhan vien nam, so nv nu, tong thuc linh
-
+//function : dem do dai ki tu
+int stringl(char *s){
+	int length;
+	for(length = 0; s[length] != '\0'; ++length);
+	return length;	
+}
+void centerstr(char *s){
+		int contentl = (int)(110-stringl(s)/2);
+		for(int i = 0; i<contentl; i++){
+		printf(" ");
+	}
+	printf("%s", s);
+}
+void blankline(int line){
+	for(int i=0; i<line; i++){
+		printf("\n");
+	}
+}
 int main(){
 	struct employee A = {
 		2313, 
 		"Phuong", 
 		"Hau"	
 	};
-	printf("%s", A.firstName);
-	printf("Hello world! ");
+	blankline(5);
+	centerstr("QUAN LI NHAN VIEN");
+	blankline(5);
+	centerstr("MENU");
 	return 0;
 }
